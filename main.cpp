1b0466c8108a77aa40c501795d579422729f8f9a
Lucas Marcondes Delfino <lucasdelfus@gmail.com>
2026-04-05 01:53:36 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -32,32 +32,70 @@ xTaskCreate (semaforoPedestre , "semaforoPedestre" , 1024 , NULL , 1 , &semaforo
 }
 
 
-void desligaTudo (){
+/// @brief Funcão responsável por desligar todos os pinos dos leds
+void desligaTudo(){
   ledcWrite(VERMELHO_CAR1_CHANNEL, 0);
-  ledcWrite(VERMELHO_CAR2_CHANNEL, 0);
-  ledcWrite(VERMELHO_PEDESTRE1_CHANNEL, 0);
-  ledcWrite(VERMELHO_PEDESTRE2_CHANNEL, 0);
   ledcWrite(VERDE_CAR1_CHANNEL, 0);
-  ledcWrite(VERDE_CAR2_CHANNEL, 0);
+  ledcWrite(VERMELHO_PEDESTRE1_CHANNEL, 0);
   ledcWrite(VERDE_PEDESTRE1_CHANNEL, 0);
-  ledcWrite(VERDE_PEDESTRE2_CHANNEL, 0);
 
+  ledcWrite(VERMELHO_CAR2_CHANNEL, 0);
+  ledcWrite(VERDE_CAR2_CHANNEL, 0);
+  ledcWrite(VERMELHO_PEDESTRE2_CHANNEL, 0);
+  ledcWrite(VERDE_PEDESTRE2_CHANNEL, 0);
 }
 
+/// @brief Liga os leds verdes do pedestre
+void ligaPedestre(){
+  ledcWrite(VERMELHO_CAR1_CHANNEL, 4095);
+  ledcWrite(VERMELHO_CAR2_CHANNEL, 4095);
+  ledcWrite(VERDE_PEDESTRE1_CHANNEL, 4095);
+  ledcWrite(VERDE_PEDESTRE2_CHANNEL, 4095);
+  ledcWrite(VERMELHO_PEDESTRE1_CHANNEL, 0);
+  ledcWrite(VERMELHO_PEDESTRE2_CHANNEL, 0);
+}
+
+/// @brief Função que faz o led ficar amarelo, indicando o semáforo amarelo
+void amareloCarro1(){
+  ledcWrite(VERMELHO_CAR1_CHANNEL, 2000);
+  ledcWrite(VERDE_CAR1_CHANNEL, 4095);
+}
+
+/// @brief Função que faz o led ficar amarelo, indicando o semáforo amarelo
+void amareloCarro2(){
+  ledcWrite(VERMELHO_CAR2_CHANNEL, 2000);
+  ledcWrite(VERDE_CAR2_CHANNEL, 4095);
+}
+
+/// @brief Função que faz o led ficar vermelho para os pedestres, indicando o semáforo amarelo
+void vermelhoPedestre(){
+  ledcWrite(VERMELHO_PEDESTRE1_CHANNEL, 4095);
+  ledcWrite(VERMELHO_PEDESTRE2_CHANNEL, 4095);
+}
+
+/// @brief Função que desliga o led dos pedestres, indicando o semáforo amarelo
+void desligaPedestre(){
+  ledcWrite(VERMELHO_PEDESTRE1_CHANNEL, 0);
+  ledcWrite(VERMELHO_PEDESTRE2_CHANNEL, 0);
+}
 
 void loop(){
-int botaoSemaforo1 = digitalRead(BOTAO_PEDESTRE1);
-int botaoSemaforo2 = digitalRead(BOTAO_PEDESTRE2);
-    if(botaoSemaforo1 && !botaoAtivo  &&  millis()- botaoMillis >= 150){
+  int botaoSemaforo1 = digitalRead(BOTAO_PEDESTRE1);
+  int botaoSemaforo2 = digitalRead(BOTAO_PEDESTRE2);
+  // 150ms de debounce
+  unsigned long debounceButton =  millis()- botaoMillis >= 150;
+
+    if(botaoSemaforo1 && !botaoAtivo && debounceButton){
       botaoAtivo = 1;
       blockBotao1 = true;
       blockBotao2 = false;
     }
-    if(botaoSemaforo2 && !botaoAtivo &&  millis() - botaoMillis >= 150){
+    if(botaoSemaforo2 && !botaoAtivo && debounceButton){
       botaoAtivo = 2;
-      blockBotao2 = true;
       blockBotao1 = false;
+      blockBotao2 = true;
     }
+
     if(botaoAtivo == 1) {
       blockBotao2 = false; 
     } else if(botaoAtivo == 2) {
@@ -71,134 +109,130 @@ void semaforoCarro(void * pv){
     if(semaforo == 0){
       botaoAtivo = 0;
     }
-        switch(semaforo){
-        case 0 : 
-          // amarelo pro carro 2
-          ledcWrite (VERMELHO_CAR1_CHANNEL , 4095);
-          vermelhoPedestre();
-          amareloCarro2 ();
-          if(millis() - tempoMillis >= 3000){
+    switch(semaforo){
+      case 0 : 
+        // amarelo pro carro 2
+        amareloCarro2();
+        vermelhoPedestre();
+        ledcWrite(VERMELHO_CAR1_CHANNEL , 4095);
+        if(millis() - tempoMillis >= 3000){
+          tempoMillis = millis();
+          ledcWrite(VERMELHO_CAR1_CHANNEL, 0);
+          ledcWrite(VERMELHO_CAR2_CHANNEL, 0);
+          ledcWrite(VERDE_CAR2_CHANNEL, 0);
+          desligaPedestre();
+          semaforo = 1;
+        }
+        break;
+
+      case 1: 
+      // vermelho pro carro 2 e verde pro carro 1 
+        ledcWrite(VERDE_CAR1_CHANNEL , 4095);
+        ledcWrite(VERMELHO_CAR2_CHANNEL , 4095);
+        vermelhoPedestre();
+        if(blockBotao1 || blockBotao2){
+          if(millis()- tempoMillis >= 3500){
             tempoMillis = millis();
+            passaPedestre1 = true;
+            botAuxilia1 = true;
+            blockBotao1 = false;
+            blockBotao2 = false;
+            semaforo = 2;
+          }
+        }
+        if(millis() - tempoMillis >= 7000){
+            tempoMillis = millis();
+            ledcWrite(VERDE_CAR1_CHANNEL, 0);
+            ledcWrite(VERMELHO_CAR2_CHANNEL, 0);
+            ledcWrite(VERMELHO_PEDESTRE1_CHANNEL, 0);
+            ledcWrite(VERMELHO_PEDESTRE2_CHANNEL, 4095);
+            semaforo = 2;
+        }
+        break;
+
+      case 2 : 
+        // amarelo pro carro 1
+        ledcWrite(VERMELHO_CAR2_CHANNEL, 4095);
+        vermelhoPedestre();
+        amareloCarro1();
+        if(millis() - tempoMillis >= 3000){
+          tempoMillis = millis();
+          if(passaPedestre1 || passaPedestre2){
+            if(eTaskGetState (semaforoPedestreHandler) != eRunning){
+              vTaskResume (semaforoPedestreHandler);
+              desligaTudo();
+            }
+            vTaskSuspend (NULL);
+          } 
+            ledcWrite(VERMELHO_CAR2_CHANNEL, 0);
+            ledcWrite(VERMELHO_CAR1_CHANNEL, 0);
+            ledcWrite(VERDE_CAR1_CHANNEL, 0);
+            desligaPedestre();
+            semaforo = 3;
+        }
+        break;
+
+      case 3:
+        // vermelho carro 1 e verde pro carro 2
+        ledcWrite(VERMELHO_CAR1_CHANNEL, 4095);
+        ledcWrite(VERDE_CAR2_CHANNEL, 4095);
+        vermelhoPedestre();
+        if(blockBotao2 || blockBotao1){
+          if(millis()- tempoMillis >= 3500){
+            tempoMillis = millis();
+            passaPedestre2 = true;
+            botAuxilia2 = true;
+            blockBotao2  = false;
+            semaforo = 4;
+          }
+        }
+        if(millis() - tempoMillis >= 7000){
+          tempoMillis = millis();
+          ledcWrite(VERMELHO_CAR1_CHANNEL, 0);
+          ledcWrite(VERDE_CAR2_CHANNEL, 0);
+          ledcWrite(VERMELHO_PEDESTRE2_CHANNEL, 0);
+          ledcWrite(VERMELHO_PEDESTRE1_CHANNEL,  4095);
+          semaforo = 0;
+        }
+        break;
+
+      case 4 :
+        ledcWrite(VERMELHO_CAR1_CHANNEL, 4095);
+        vermelhoPedestre();
+        amareloCarro2();
+        if(millis() - tempoMillis >= 3000){
+            tempoMillis = millis ();
+            if (passaPedestre2 || passaPedestre1){
+              if (eTaskGetState (semaforoPedestreHandler) != eRunning){
+                vTaskResume (semaforoPedestreHandler);
+                desligaTudo();
+              }
+              vTaskSuspend(NULL);
+            }
             ledcWrite(VERMELHO_CAR1_CHANNEL, 0);
             ledcWrite(VERMELHO_CAR2_CHANNEL, 0);
             ledcWrite(VERDE_CAR2_CHANNEL, 0);
             desligaPedestre();
             semaforo = 1;
-          }
-          break;
-
-        case 1: 
-        // vermelho pro carro 2 e verde pro carro 1 
-          ledcWrite(VERDE_CAR1_CHANNEL , 4095);
-          ledcWrite(VERMELHO_CAR2_CHANNEL , 4095);
-          vermelhoPedestre();
-          if(blockBotao1 || blockBotao2){
-            if(millis()- tempoMillis >= 3500){
-              tempoMillis = millis();
-              passaPedestre1 = true;
-              botAuxilia1 = true;
-              blockBotao1 = false;
-              semaforo = 2;
-            }
-          }
-          if(millis() - tempoMillis >= 7000){
-              tempoMillis = millis();
-              ledcWrite(VERDE_CAR1_CHANNEL, 0);
-              ledcWrite(VERMELHO_CAR2_CHANNEL, 0);
-              ledcWrite(VERMELHO_PEDESTRE1_CHANNEL, 0);
-              ledcWrite(VERMELHO_PEDESTRE2_CHANNEL, 4095);
-              semaforo = 2;
-          }
-          break;
-
-        case 2 : 
-          // amarelo pro carro 1
-          ledcWrite(VERMELHO_CAR2_CHANNEL, 4095);
-          vermelhoPedestre();
-          amareloCarro1();
-          if(millis() - tempoMillis >= 3000){
-            tempoMillis = millis();
-            if(passaPedestre1 || passaPedestre2){
-              if(eTaskGetState (semaforoPedestreHandler) != eRunning){
-                vTaskResume (semaforoPedestreHandler);
-                desligaTudo();
-              }
-              vTaskSuspend (NULL);
-            } 
-              ledcWrite(VERMELHO_CAR2_CHANNEL, 0);
-              ledcWrite(VERMELHO_CAR1_CHANNEL, 0);
-              ledcWrite(VERDE_CAR1_CHANNEL, 0);
-              desligaPedestre();
-              semaforo = 3;
-          }
-          break;
-
-        case 3:
-          // vermelho carro 1 e verde pro carro 2
-          ledcWrite(VERMELHO_CAR1_CHANNEL, 4095);
-          ledcWrite(VERDE_CAR2_CHANNEL, 4095);
-          vermelhoPedestre();
-          if(blockBotao2 || blockBotao1){
-            if(millis()- tempoMillis >= 3500){
-              tempoMillis = millis();
-              passaPedestre2 = true;
-              botAuxilia2 = true;
-              blockBotao2  = false;
-              semaforo = 4;
-            }
-          }
-          if(millis() - tempoMillis >= 7000){
-            tempoMillis = millis();
-            ledcWrite(VERMELHO_CAR1_CHANNEL, 0);
-            ledcWrite(VERDE_CAR2_CHANNEL, 0);
-            ledcWrite(VERMELHO_PEDESTRE2_CHANNEL, 0);
-            ledcWrite(VERMELHO_PEDESTRE1_CHANNEL,  4095);
-            semaforo = 0;
-          }
-          break;
-
-        case 4 :
-          ledcWrite(VERMELHO_CAR1_CHANNEL, 4095);
-          vermelhoPedestre();
-          amareloCarro2();
-          if(millis() - tempoMillis >= 3000){
-              tempoMillis = millis ();
-              if (passaPedestre2 || passaPedestre1){
-                if (eTaskGetState (semaforoPedestreHandler) != eRunning){
-                  vTaskResume (semaforoPedestreHandler);
-                  desligaTudo();
-                }
-                vTaskSuspend(NULL);
-              }
-              ledcWrite(VERMELHO_CAR1_CHANNEL, 0);
-              ledcWrite(VERMELHO_CAR2_CHANNEL, 0);
-              ledcWrite(VERDE_CAR2_CHANNEL, 0);
-              desligaPedestre();
-              semaforo = 1;
-          }
-          break;
-          }
-          delay(10);     
+        }
+        break;
     }
+      delay(10);     
+  }
 }
 
 
 void semaforoPedestre(void * pv){
   vTaskSuspend(NULL);
   while(1){
-      switch(semaforo1){
+      switch(pedestreSemaforo){
       case 0 :
-        ledcWrite(VERMELHO_CAR1_CHANNEL, 4095);
-        ledcWrite(VERMELHO_CAR2_CHANNEL, 4095);
-        ledcWrite(VERDE_PEDESTRE1_CHANNEL, 4095);
-        ledcWrite(VERDE_PEDESTRE2_CHANNEL, 4095);
-        ledcWrite(VERMELHO_PEDESTRE1_CHANNEL, 0);
-        ledcWrite(VERMELHO_PEDESTRE2_CHANNEL, 0);
-          if(millis () - tempoMillis >= 5000){
-            tempoMillis = millis();
-            semaforo1 = 1;
-            ledPedestreDesliga();
-          }
+        ligaPedestre();
+        if(millis () - tempoMillis >= 5000){
+          tempoMillis = millis();
+          pedestreSemaforo = 1;
+          desligaPedestre();
+        }
         break;
 
       case 1 :
@@ -206,13 +240,10 @@ void semaforoPedestre(void * pv){
         ledcWrite(VERMELHO_CAR2_CHANNEL, 4095);
           if(millis() - ledMillisBlink >= 400){
             ledMillisBlink = millis();
-            if(ledcRead(VERMELHO_PEDESTRE1_CHANNEL) <= 0 && ledcRead(VERMELHO_PEDESTRE2_CHANNEL) <= 0){
-              ledcWrite(VERMELHO_PEDESTRE1_CHANNEL, 4095);
-              ledcWrite(VERMELHO_PEDESTRE2_CHANNEL, 4095);
-            } else {
-              ledcWrite(VERMELHO_PEDESTRE1_CHANNEL, 0);
-              ledcWrite(VERMELHO_PEDESTRE2_CHANNEL, 0);
-            }
+            static bool ledState = false;
+            ledState = !ledState;
+            ledcWrite(VERMELHO_PEDESTRE1_CHANNEL, ledState ? 4095 : 0);
+            ledcWrite(VERMELHO_PEDESTRE2_CHANNEL, ledState ? 4095 : 0);
           }
           if(millis() - tempoMillis >= 3000){
               tempoMillis = millis();
@@ -235,7 +266,7 @@ void semaforoPedestre(void * pv){
                     vTaskResume(semaforoCarroHandler);
                   }
               }
-              semaforo1 = 0;
+              pedestreSemaforo = 0;
               vTaskSuspend(NULL);
           }
           break;
@@ -243,7 +274,3 @@ void semaforoPedestre(void * pv){
       delay(10);
   }
 }
-
-
-
-
